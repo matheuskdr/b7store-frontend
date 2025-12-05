951d20d0579c3ee5f3661b1d98b9cf64f419edf6
matheuskdr <matheusgabriellkpo@gmail.com>
2025-12-05 16:54:01 -0300

    Header menu mobile

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,8 +3,20 @@
 import Image from "next/image";
 import { HeaderIcon } from "./header-icon";
 import Link from "next/link";
+import { useState } from "react";
+
+type MenuItem = {
+    label: string;
+    href: string;
+};
 
 export function Header() {
+    const menu: MenuItem[] = [
+        { label: "Camisa", href: "/categories/camisa" },
+        { label: "Kits", href: "/categories/kits" },
+    ];
+    const [menuOpened, setMenuOpened] = useState(false);
+
     return (
         <header className="bg-white border-b border-gray-200">
             <div className="bg-black text-white text-center p-4">
@@ -14,12 +26,14 @@ export function Header() {
             <div className="w-full max-w-6xl mx-auto p-6">
                 <div className="flex items-center justify-between">
                     <div className="">
-                        <Image
-                            src="/assets/ui/logo-black.png"
-                            alt="B7Store"
-                            width={120}
-                            height={40}
-                        />
+                        <Link href={"/"}>
+                            <Image
+                                src="/assets/ui/logo-black.png"
+                                alt="B7Store"
+                                width={120}
+                                height={40}
+                            />
+                        </Link>
                     </div>
                     <div className="">
                         <div className="flex gap-4">
@@ -35,16 +49,41 @@ export function Header() {
                                     alt={"Carrinho"}
                                 />
                             </Link>
-                            <div className="md:hidden">
+                            <div
+                                className="md:hidden"
+                                onClick={() => setMenuOpened(!menuOpened)}
+                            >
                                 <HeaderIcon
                                     src={"/assets/ui/menu-line.png"}
                                     alt={"Menu"}
+                                    selected={menuOpened}
+                                    srcSelected="/assets/ui/menu-line-white.png"
                                 />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            {menuOpened && (
+                <div className="md:hidden">
+                    {menu.map((item) => (
+                        <Link key={item.label} href={item.href}>
+                            <div className="p-6 border-b border-gray-200 flex items-center justify-between">
+                                <div className="font-medium text-lg text-gray-500">
+                                    {item.label}
+                                </div>
+                                <Image
+                                    src={"/assets/ui/arrow-up-right.png"}
+                                    alt="Ir a categoria"
+                                    width={24}
+                                    height={24}
+                                />
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
+            <div className="p-6 md:hidden">Busca mobile</div>
         </header>
     );
 }
