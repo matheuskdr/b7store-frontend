f28ff59724b15db6df9cb2c567c9697d8a85683c
matheuskdr <matheusgabriellkpo@gmail.com>
2025-12-08 11:52:50 -0300

    Header search input

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { HeaderIcon } from "./header-icon";
 import Link from "next/link";
 import { useState } from "react";
+import { HeaderSearch } from "./header-search";
 
 type MenuItem = {
     label: string;
@@ -83,7 +84,9 @@ export function Header() {
                     ))}
                 </div>
             )}
-            <div className="p-6 md:hidden">Busca mobile</div>
+            <div className="p-6 md:hidden">
+                <HeaderSearch />
+            </div>
         </header>
     );
 }
